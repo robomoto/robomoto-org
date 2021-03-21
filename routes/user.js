11f527a64d79585c75e8a92dc2119a9bbb84dc65
robomoto <31261450+robomoto@users.noreply.github.com>
2021-03-21 12:53:28 -0700

    removed registration

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ const users = require('../controllers/users');
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
 
-router.route('/register')
-    .get(users.renderRegistrationForm)
-    .post(wrapAsync(users.registerUser))
+// router.route('/register')
+//     .get(users.renderRegistrationForm)
+//     .post(wrapAsync(users.registerUser))
 
 router.route('/login')
     .get(users.renderLoginForm)
